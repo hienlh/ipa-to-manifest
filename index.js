1bf3087e79784b926ae94332fcd2a4620c7d40a0
Hieren <hienlh1298@gmail.com>
2020-09-06 18:13:23 +0700

    fix: fix async func

--- index.js
+++ index.js
@@ -4,50 +4,54 @@ const pkgReader = require("reiko-parser");
 const fs = require("fs");
 const { manifestContentMaker } = require("./manifest");
 
-const github = new GitHub(process.env.GITHUB_TOKEN);
+const main = async () => {
+  const github = new GitHub(process.env.GITHUB_TOKEN);
 
-const { owner, repo } = context.repo;
+  const { owner, repo } = context.repo;
 
-const tagName = context.ref;
+  const tagName = context.ref;
 
-const tag = tagName.replace("refs/tags/", "");
+  const tag = tagName.replace("refs/tags/", "");
 
-const getReleaseResponse = await github.repos.getReleaseByTag({
-  owner,
-  repo,
-  tag,
-});
+  const getReleaseResponse = await github.repos.getReleaseByTag({
+    owner,
+    repo,
+    tag,
+  });
 
-const {
-  data: { upload_url: uploadUrl },
-} = getReleaseResponse;
+  const {
+    data: { upload_url: uploadUrl },
+  } = getReleaseResponse;
 
-const ipaPath = core.getInput("ipaPath");
+  const ipaPath = core.getInput("ipaPath");
 
-const fileName = ipaPath.split(".")[0];
+  const fileName = ipaPath.split(".")[0];
 
-const iconPath = fileName + ".png";
-const manifestPath = fileName + ".plist";
+  const iconPath = fileName + ".png";
+  const manifestPath = fileName + ".plist";
 
-const data = new pkgReader(ipaPath, "ipa", { withIcon: true });
-data.parse((err, pkgInfo) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(uploadUrl);
-    fs.writeFile(iconPath, pkgInfo.icon.split(";base64,").pop(), {
-      encoding: "base64",
-    });
-    fs.writeFile(
-      manifestPath,
-      manifestContentMaker(ipaPath, {
-        iconUrl: iconUrl,
-        bundleId: pkgInfo.CFBundleIdentifier,
-        title: pkgInfo.CFBundleName,
-        version: pkgInfo.CFBundleShortVersionString,
-      })
-    );
+  const data = new pkgReader(ipaPath, "ipa", { withIcon: true });
+  data.parse((err, pkgInfo) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(uploadUrl);
+      fs.writeFile(iconPath, pkgInfo.icon.split(";base64,").pop(), {
+        encoding: "base64",
+      });
+      fs.writeFile(
+        manifestPath,
+        manifestContentMaker(ipaPath, {
+          iconUrl: iconUrl,
+          bundleId: pkgInfo.CFBundleIdentifier,
+          title: pkgInfo.CFBundleName,
+          version: pkgInfo.CFBundleShortVersionString,
+        })
+      );
 
-    core.setOutput('manifestPath', manifestPath);
-  }
-});
+      core.setOutput("manifestPath", manifestPath);
+    }
+  });
+};
+
+main();
